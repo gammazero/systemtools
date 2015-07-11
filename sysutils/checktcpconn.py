b95a2a3ee86bb5264cb1ba4a0abf7a36b7864d29
Andrew Gillis <andrew.gillis@creativeautomata.com>
2015-07-10 17:04:31 -0700

    Use argparse when run on commandline.

--- sysutils/checktcpconn.py
+++ sysutils/checktcpconn.py
@@ -100,41 +100,36 @@ class CheckTcpConn(object):
         return self._conn_info[0]
 
 
-if __name__ == '__main__':
-    # Check that command-line argument was given.
-    usage = 'usage: %s host port [--wait timeout] [--poll]' % (sys.argv[0],)
-    if len(sys.argv) < 3:
-        print(usage, file=sys.stderr)
-        sys.exit(1)
+def main():
+    import argparse
+    ap = argparse.ArgumentParser(
+        description='Check if a TCP connection can be made to a host.')
+    ap.add_argument('host',
+                    help='IP address or DNS name of host to connect to.')
+    ap.add_argument('port', type=int, help='Port to connect to host on.')
+    ap.add_argument('--poll', action='store_true',
+                    help='Try repeatedly to make a TCP connection until '
+                    'timeout.')
+    ap.add_argument('--wait', metavar='seconds', type=float, dest='timeout',
+                    default=60.0,
+                    help='Time in seconds to wait for connection.')
+    args = ap.parse_args()
 
-    argv = list(sys.argv)
-    prg = argv.pop(0)
-    addr = argv.pop(0)
-    port = argv.pop(0)
-
-    timeout = 60.0
-    poll = False
-    while argv:
-        arg = argv.pop(0)
-        if arg == '--wait':
-            if not argv:
-                print('missing timeout after --wait', file=sys.stderr)
-                print(usage, file=sys.stderr)
-                sys.exit(1)
-            timeout = float(argv.pop(0))
-        elif arg == '--poll':
-            poll = True
-
-    tcp_chk = CheckTcpConn(addr, port)
-    if poll:
-        if not tcp_chk.poll(timeout):
+    tcp_chk = CheckTcpConn(args.host, args.port)
+    if args.poll:
+        if not tcp_chk.poll(args.timeout):
             print('timed out waiting to connect:', tcp_chk.last_error(),
                   file=sys.stdout)
-            sys.exit(1)
+            return 1
     else:
-        if not tcp_chk.try_connect(timeout):
+        if not tcp_chk.try_connect(args.timeout):
             print('unable to connect:', tcp_chk.last_error(),
                   file=sys.stdout)
-            sys.exit(1)
+            return 1
 
-    print('connected to %s:%s' % (addr, port))
+    print('connected to %s:%s' % (args.host, args.port))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
